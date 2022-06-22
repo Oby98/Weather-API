dbe9d761a07ac884ab626eb30110e74e4c3e9a66
Oby98 <obaidurehman98@gmail.com>
2022-06-22 14:55:28 +0500

    Requirments are completed

--- weather.rb
+++ weather.rb
@@ -5,23 +5,33 @@ require 'json'
 def get_median(arr)
   sorted = arr.sort
   mid = sorted.length/2
-  sorted[mid-1,2].sum/2.0
+  (sorted[mid-1,2].sum/2.0).round(1)
 end
 
 cities_data = []
-uri = URI("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Karachi,%20Pakistan/last29days?unitGroup=us&include=days&key=WQQ3LTJVN6Y6SV623RCLEK6RK&contentType=json")
-params = { :unitGroup => 'us', :include => 'days', :key => "WQQ3LTJVN6Y6SV623RCLEK6RK", :contentType => "json" }
+cities = [['Copenhagen','Denmark'], ['Lodz','Poland'], ['Brussels','Belgium'], ['Islamabad','Pakistan']]
 
-uri.query = URI.encode_www_form(params)
-res = Net::HTTP.get_response(uri)
+puts "city        wind_avg    wind_median     temp_avg     temp_median "
+cities.each do|city,country|
 
-data = JSON.parse(res.body)
+  endpoint = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/#{city},%20#{country}/last29days?unitGroup=us&include=days&key=WQQ3LTJVN6Y6SV623RCLEK6RK&contentType=json"
+  uri = URI(endpoint)
+  params = { :unitGroup => 'us', :include => 'days', :key => "WQQ3LTJVN6Y6SV623RCLEK6RK", :contentType => "json" }
 
-days_temp = data['days'].collect{|day| day["temp"]}
-days_speed = data['days'].collect{|day| day["windspeed"]}
+  uri.query = URI.encode_www_form(params)
+  res = Net::HTTP.get_response(uri)
 
-temp_median = get_median(days_temp)
-speed_median = get_median(days_speed)
+  data = JSON.parse(res.body)
 
-temp_avg = (days_tmp.sum(2)/days_tmp.size).round(2)
-speed_avg = (days_speed.sum(2)/days_speed.size).round(2)
+  days_temp = data['days'].collect{|day| day["temp"]}
+  days_speed = data['days'].collect{|day| day["windspeed"]}
+
+  temp_median = get_median(days_temp)
+  speed_median = get_median(days_speed)
+
+  temp_avg = (days_temp.sum(2)/days_temp.size).round(1)
+  speed_avg = (days_speed.sum(2)/days_speed.size).round(1)
+
+  puts "#{city}, #{country}        #{speed_avg}    #{speed_median}     #{temp_avg}     #{temp_median}"
+
+end
